ffa61ca3629535f56dd293fd76fc2a1e0f0fb45d
luango007 <foxbr007g@gmail.com>
2025-11-13 10:34:53 -0300

    f

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("primeiro app")
 
 st.header("vamos fazer algo")
-n =st.number_imput('escolhar um numero')
+n =st.number_input('escolhar um numero')
 st.write(f"o numero que voce escolheu ao quadrado e (n**2)")
